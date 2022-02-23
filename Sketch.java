9628bad5c8b38772b68beee449146f6fd9e63e21
Emily Ma <99974145+emily0412@users.noreply.github.com>
2022-02-23 23:09:32 +0000

    save

--- Sketch.java
+++ Sketch.java
@@ -2,34 +2,66 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
 	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //chimney
+    stroke(170, 64, 105);
+    strokeWeight(3);
+    fill(168, 81, 110);
+    rect(width/((float)80/27), height/((float)80/11), width/20, height/((float)20/3));
+    
+    //house
+    stroke(241, 156, 187);
+    strokeWeight(3);
+    fill(250, 218, 221);
+    rect(width/((float)10/3), height/((float)80/27), width/((float)5/2), height/((float)200/81));
+
+    //roof
+    stroke(181, 114, 129);
+    strokeWeight(3);
+    fill(217, 134, 149);
+    triangle(width/((float)80/19), height/((float)80/27), width/20, height/8, width/((float)80/61), height/((float)80/27));
+
+    //window
+    stroke(192, 128, 129);
+    strokeWeight(3);
+    fill(204, 259, 255);
+    ellipse(200, 175, 30, 30);
+
+    //door
+    stroke(192, 128, 129);
+    strokeWeight(3);
+    fill(239, 187, 204);
+    rect(210, 235, 40, 62);
+
+    //doorknob
+    fill(165, 42, 42);
+    noStroke();
+    ellipse(240, 265, 7, 7);
+
+    //ground colour
+    fill(163, 193, 173);
+    noStroke();
+    rect(0, 294, 400, 294);
+
+    //ground first line
+    stroke(165, 42, 42);
+    strokeWeight(7);
+    line(70, 300, 330, 300);
+    
+    //ground second line
+    stroke(165, 42, 42);
+    strokeWeight(7);
+    line(100, 330, 300, 330);
   }
   
   // define other methods down here.
